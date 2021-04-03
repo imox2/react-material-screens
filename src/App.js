96bc61fb89ab10637103c382e7b6d0917be8754e
Talha <talhaanwar.anwar@gmail.com>
2021-04-03 16:16:11 +0530

    Used material ui to do a bit of styling

--- src/App.js
+++ src/App.js
@@ -6,19 +6,57 @@ import {
 import  PaymentInformation  from "./pages/paymentInformation";
 import  ReviewOrder  from "./pages/reviewOrder";
 import  TermsAndConditions  from "./pages/termsandconditions";
+import {withStyles,makeStyles} from "@material-ui/core/styles";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
-function App() {
+const theme = createMuiTheme({
+    typography: {
+        fontSize: 12,
+        fontFamily: '"Work Sans"'
+    },
+    fontFamily: '"Work Sans"',
+})
+// const styles = theme => ( {
+//   root: {
+//     'marginLeft': '8rem',
+//     'marginRight': '8rem',
+//   },
+// } );
+
+const styles = makeStyles((theme) => ({
+  faisal: {
+    [theme.breakpoints.down("sm")]: {
+      marginRight: '1rem',
+      marginLeft: '1rem',
+    },
+    [theme.breakpoints.up("md")]: {
+      backgroundColor: theme.palette.primary.main,
+    },
+    [theme.breakpoints.up("lg")]: {
+      marginRight: '8rem',
+      marginLeft: '8rem',
+    },
+  },
+}));
+
+
+function App(props) {
+  const classes = styles();
+  console.log(classes);
   return (
-    <div>
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.faisal}>
        <Router>
        <Switch>
+                
               <Route component={PaymentInformation} path="/payment" />
               <Route component={ReviewOrder} path="/order" />
               <Route component={TermsAndConditions} path="/tnc" />
-              <Route component={TermsAndConditions} path="/" />
-            </Switch>
+              <Route component={PaymentInformation} path="/" />
+        </Switch>
       </Router>
-    </div>
+      </div>
+      </MuiThemeProvider>
   );
 }
 
--- src/pages/paymentInformation.js
+++ src/pages/paymentInformation.js
@@ -1,10 +1,26 @@
 import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
 
+const styles = makeStyles((theme) => ({
+    root: {
+      [theme.breakpoints.down("sm")]: {
+        backgroundColor: theme.palette.secondary.main,
+      },
+      [theme.breakpoints.up("md")]: {
+        backgroundColor: theme.palette.primary.main,
+      },
+      [theme.breakpoints.up("lg")]: {
+        backgroundColor: "green",
+      },
+    },
+}));
+
+  
 function PaymentInformation () {
-
+    const classes = styles();
     return(
         <>
-        This is Payment
+        <h3 className={classes.root}>Payment Information</h3>
         </>
     )
 }
